ccd429f21086864c244434c1733cf35f0ba58e15
Chintanneosoft <chintan.rajgor@neosoftmail.com>
2023-11-17 09:57:50 +0530

    Initial Commit

---
+++ Location and Maps/Location_and_MapsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Location_and_MapsApp.swift
+//  Location and Maps
+//
+//  Created by Neosoft on 17/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Location_and_MapsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
